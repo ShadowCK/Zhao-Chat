ab8c8cbded210c5a918dc86317f2cde2cc4715a3
ShadowCK <540712348@qq.com>
2023-10-22 15:24:10 -0400

    Added nodemon

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 // Home page
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 // CSS for the home page
-const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
